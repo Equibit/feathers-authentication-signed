e6be4bdc113a97f6437468a7551c17b77ffdb6e4
Marshall Thompson <marshall@creativeideal.net>
2017-03-24 09:47:25 -0600

    Add first client util

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 // import errors from 'feathers-errors';
 import makeDebug from 'debug';
+import crypto from 'crypto';
 
 const debug = makeDebug('feathers-authentication-signed');
 
@@ -7,3 +8,10 @@ export default function init () {
   debug('Initializing feathers-authentication-signed plugin');
   return 'feathers-authentication-signed';
 }
+
+init.generateSecret = function generateSecret (password, salt) {
+  crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
+    if (err) throw err;
+    console.log(key.toString('hex'));
+  });
+};
