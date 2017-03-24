b0b2595589b783a11f23582ac5c8ca2f9f015fc1
Marshall Thompson <marshall@creativeideal.net>
2017-03-24 13:40:21 -0600

    Add client utilities.

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,42 @@
-// import errors from 'feathers-errors';
-import makeDebug from 'debug';
-import crypto from 'crypto';
-
-const debug = makeDebug('feathers-authentication-signed');
-
-export default function init () {
-  debug('Initializing feathers-authentication-signed plugin');
-  return 'feathers-authentication-signed';
-}
-
-init.generateSecret = function generateSecret (password, salt) {
-  crypto.pbkdf2(password, salt, 100000, 512, 'sha512', (err, key) => {
-    if (err) throw err;
-    console.log(key.toString('hex'));
-  });
+const defaults = {
+  pbkdf2: undefined, // Must be passed in
+  createHmac: undefined // Must be passed in
 };
+
+export default function plugin (options) {
+  options = Object.assign({}, defaults, options);
+
+  if (!options.pbkdf2) {
+    throw new Error('You must provide a `pbkdf2` function to the feathers-authentication-signed plugin options');
+  }
+  if (!options.createHmac) {
+    throw new Error('You must provide a `createHmac` function to the feathers-authentication-signed plugin options');
+  }
+
+  function signed () {
+
+  }
+
+  let utils = {
+    generateSecret (password, salt) {
+      return new Promise((resolve, reject) => {
+        options.pbkdf2(password, salt, 250, 512, 'sha512', function (err, key) {
+          if (err) return reject(err);
+          resolve(key.toString('hex'));
+        });
+      });
+    },
+    sign (data, secret) {
+      return new Promise((resolve, reject) => {
+        let dataString = JSON.stringify(data);
+        let hmac = options.createHmac('sha512', secret);
+        let signature = hmac.update(dataString).digest('hex');
+        data.signature = signature;
+        resolve(data);
+      });
+    }
+  };
+
+  Object.assign(signed, utils);
+  return signed;
+}
